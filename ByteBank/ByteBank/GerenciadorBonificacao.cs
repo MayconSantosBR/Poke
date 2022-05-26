a2f0b408cd472259c54b9e52d5bb9b2a73bcae50
MayconSantosBR <maycon.wb.santos@gmail.com>
2022-05-26 19:15:12 -0300

    curso alura backup

---
+++ ByteBank/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,22 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonifacacao { get; set; }
+
+        public void Registrar(Funcionario funcionario) {
+            _totalBonifacacao += funcionario.GetBonificacao();
+        }
+
+        public double GetBonificacao() { 
+            return _totalBonifacacao; 
+        }
+    }
+}
